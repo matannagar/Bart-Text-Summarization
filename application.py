6058c617c17e16f8790519548cbe69c54fa06eec
matannagar <73398186+matannagar@users.noreply.github.com>
2021-11-02 14:56:24 +0200

    added documentation , fixed url link

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 """
 Web-App based on flask library
+connecting the python-server to the index.html page (located inside templates)
 """
 from flask import *
 from flask import request
@@ -18,7 +19,19 @@ app.config['UPLOAD_PATH'] = 'C:/Users/public'
 app.config['UPLOAD_EXTENSIONS'] = ['.docx', '.doc', '.txt', '.pdf', '.html']
 app.secret_key = "super secret key"
 
-# homepage
+
+def process_file(f, percentage, max_words):
+    # possible method of reading file directly : input_data = f.stream.read().decode("utf-8"), not working with docx
+    filename = secure_filename(f.filename)
+    if filename != '':
+        file_ext = os.path.splitext(filename)[1]
+        # verify type of file (docx, html, txt, pdf)
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']:
+            return render_template("index.html", error="Only docx,txt,pdf,html allowed!")
+        path = os.path.join(app.config['UPLOAD_PATH'], filename)
+        f.save(path)
+
+        return path
 
 
 @app.route('/')
@@ -30,6 +43,7 @@ def upload():
 def index(glob_var="", dictionary=""):
     if request.method == 'POST':
         print("started flask")
+        # retrive user input for max words or wanted percentages of summary
         max_words = request.form['max-words']
         if max_words != '':
             max_words = int(max_words)
@@ -37,40 +51,36 @@ def index(glob_var="", dictionary=""):
         percentage = request.form['percentage']
         if percentage != '':
             percentage = int(percentage)
-
+        print("I am here")
+        # load excel-entities file and translate it into a python dictionary
         dictionary = turn_into_dictionary()
 
         # attempt to get file1
         f = request.files['file1']
-        if secure_filename(f.filename) == '':
+        if secure_filename(f.filename) != '':
+            path = process_file(f, percentage, max_words)
+            summary = summarize(path, percentage, max_words)
+            glob_var = summary
+            return render_template("index.html", name=f.filename, summary=summary, dictionary=dictionary)
             # attempt to get file2
+        elif secure_filename(request.files['file2'].filename) != '':
             f = request.files['file2']
-            if secure_filename(f.filename) != '':
-                # possible method of reading file directly : input_data = f.stream.read().decode("utf-8"), not working with docx
-                filename = secure_filename(f.filename)
-                if filename != '':
-                    file_ext = os.path.splitext(filename)[1]
-                    # verify type of file
-                    if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-                        return render_template("index.html", error="Only .docx or .txt allowed!")
-                    path = os.path.join(app.config['UPLOAD_PATH'], filename)
-                    f.save(path)
-
-                    summary = summarize(path, percentage, max_words)
-                    glob_var = summary
-
-                    return render_template("index.html", name=f.filename, summary=summary, dictionary=dictionary)
-            # attempt to get URL
-            else:
-                f = request.form['input_url']
-                if f != '':
-                    summary = summarize_from_web(f, percentage, max_words)
-                    return render_template("index.html", name="web", summary=summary, dictionary=dictionary)
-                else:  # if no file was chosen
-                    print("no file chosen")
-                    return render_template("index.html", error="You have to pick a file!")
+            path = process_file(f, percentage, max_words)
+            summary = summarize(path, percentage, max_words)
+            glob_var = summary
+            return render_template("index.html", name=f.filename, summary=summary, dictionary=dictionary)
+        # attempt to get URL
+        else:
+            f = request.form['input_url']
+            if f != '':
+                summary = summarize_from_web(f, percentage, max_words)
+                return render_template("index.html", name="web", summary=summary, dictionary=dictionary)
+            else:  # if no file was chosen
+                print("no file chosen")
+                return render_template("index.html", error="You have to pick a file!")
             # session['my_var'] = summary
             # redirect(url_for('entity_tree'))
+    # load page from shared link
     else:
         return render_template("index.html", summary=glob_var)
 
--- summarize.py
+++ summarize.py
@@ -1,3 +1,16 @@
+'''
+the summarize file is responsible for downloading and using the BART NLP Model
+https://huggingface.co/facebook/bart-large-cnn
+this model has been trained on facebook and can be used for text-summarization
+
+3 key functions in this file:
+    1. summary_length : resposible for calculating percentages of the article's length
+    2. words_in_summary: how many words approx will appear in the summary
+    3. bart model : uses the model as mentioned above and returns a text (summarization)
+    4. summarize : summarizes from either txt,docx, pdf, html...
+    5. summarize_from_web: summarizes directly from URL link
+'''
+
 import docx2txt
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import re
@@ -27,25 +40,32 @@ def word_in_summary_fun(percentage, min_words, text):
 
 
 def bart_model(text, words_in_summary):
-    tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
-    model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn")
+    print("in bart")
+    try:
+        tokenizer = AutoTokenizer.from_pretrained("facebook/bart-large-cnn")
+        model = AutoModelForSeq2SeqLM.from_pretrained(
+            "facebook/bart-large-cnn")
 
-    article_input_ids = \
-        tokenizer.encode(text, return_tensors='pt', truncation=True)
-    outputs = model.generate(article_input_ids,
-                             num_beams=4,
-                             length_penalty=2.0,
-                             min_length=words_in_summary,
-                             max_length=words_in_summary + 50,
-                             no_repeat_ngram_size=3)
+        article_input_ids = \
+            tokenizer.encode(text, return_tensors='pt', truncation=True)
+        outputs = model.generate(article_input_ids,
+                                 num_beams=4,
+                                 length_penalty=2.0,
+                                 min_length=words_in_summary,
+                                 max_length=words_in_summary + 50,
+                                 no_repeat_ngram_size=3)
 
-    summary = tokenizer.decode(outputs[0])
-    summary = summary[7:-4]
+        summary = tokenizer.decode(outputs[0])
+        summary = summary[7:-4]
 
-    return summary
+        return summary
+    except:
+        return "A problem occured while summarizing."
 
 
 def summarize(filename, percentage, min_words):
+    # determine which file we are handling
+    # send the file to the suitable algorithm to "clean" the text
     if filename[-4:] == "docx":
         text = docx2txt.process(filename)
     elif filename[-4:] == "html":
@@ -55,12 +75,12 @@ def summarize(filename, percentage, min_words):
     else:
         with open(filename, 'r') as file:
             text = file.read()
-    print("working")
+
     # remove links, specials signs, emails...
     text = clean_text(text)
     # calculate how many words user want's in his summary
     words_in_summary = word_in_summary_fun(percentage, min_words, text)
-
+    print("working")
     return bart_model(text, words_in_summary)
 
 
