fa02cd5cf09bfa2e8639e4f54b0546ef7019d99e
matannagar <73398186+matannagar@users.noreply.github.com>
2022-08-05 18:26:00 +0300

    hugging face API working with Axios post

--- server/controllers/hfController.js
+++ server/controllers/hfController.js
@@ -1,22 +1,39 @@
 const axios = require('axios');
 require("dotenv").config();
+
 const api = {
     key: process.env.HF_KEY,
     base: "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
 }
 
 async function huggingfaceAPI(data) {
-    const response = await axios.get(
-        api.base,
-        {
-            headers: { Authorization: `Bearer ${api.key}` },
-            method: "POST",
-            body: JSON.stringify(data),
-        }
-    );
-    return response;
+    // console.log(data);
+    // const response = await axios(
+    //     api.base,
+    //     {
+    //         headers: { Authorization: "Bearer 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" },
+    //         method: "POST",
+    //         body: JSON.stringify(data),
+    //     }
+    // ).then((res) => {
+    //     console.log((res.data));
+    //     return res.data;
+    // });
+
+    axios({
+        method: 'post',
+        url: api.base,
+        headers: { Authorization: "Bearer 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" },
+        data: data,
+    }).then((resp => {
+        console.log(resp.data);
+        return resp.data;
+    }))
+
 }
 
 module.exports = {
     huggingfaceAPI
 }
+
+
--- server/controllers/summarizerController.js
+++ server/controllers/summarizerController.js
@@ -3,17 +3,15 @@ const huggingface = require('./hfController');
 var qs = require('querystring');
 
 const getSummary = async (req, res) => {
-  console.log(req.body.text)
+  const text = req.body.text;
 
-  // console.log(req.method);
-
-  // let result;
-  // try {
-  //   result = await huggingface.huggingfaceAPI({ inputs: text });
-  //   res.json(result.data);
-  // } catch (err) {
-  //   console.error(`failed summary api due to ${err.message}`)
-  // }
+  let result;
+  try {
+    result = await huggingface.huggingfaceAPI({ "inputs": text });
+    res.json(result);
+  } catch (err) {
+    console.error(`failed summary api due to ${err.message}`)
+  }
 
 }
 
