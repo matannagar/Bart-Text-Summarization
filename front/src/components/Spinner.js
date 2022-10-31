a4cc69f4a47f9a03fc925cb5973f1fd287c8492a
Matan Ben Nagar <matannnagar@gmail.com>
2022-10-31 12:05:38 +0200

    added spinner component

---
+++ front/src/components/Spinner.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import spinner from '../assets/spinner.gif'
+function Spinner() {
+    return (
+        <img src={spinner} alt="loading..." />
+    )
+}
+
+export default Spinner
