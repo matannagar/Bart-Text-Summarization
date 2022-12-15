67f073c019afd8dab248a4139734e57ee0c1155c
Matan Ben Nagar <matannnagar@gmail.com>
2022-12-15 21:20:31 +0200

    new contollers names

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express')
 
 const router = express.Router();
-const bartSummarizer = require('../controllers/summarizerController')
-const pyParser = require('../controllers/parserController')
+const bartSummarizer = require('../controllers/summarizer')
+const pyParser = require('../controllers/parser')
 const fileHandler = require('../middlewares/handleFile')
 
 router.post("/summarize", bartSummarizer.getSummary)
 router.post("/parser", fileHandler.upload.single("file"), pyParser.parse)
 router.post("/webparser", pyParser.webParser)
-
 
 module.exports = router;
