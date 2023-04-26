f355d59d57bf0ec6430943894fc8f495cdb5e8d7
Matan Ben Nagar <matannnagar@gmail.com>
2023-04-26 20:37:11 +0300

    handle file differently

--- server/middlewares/handleFile.js
+++ server/middlewares/handleFile.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, 'Files')
+		cb(null, path.join(__dirname, 'Files'))
 	},
 	filename: (req, file, cb) => {
 		cb(null, Date.now() + path.extname(file.originalname))
